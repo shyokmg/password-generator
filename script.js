5f9f52a315bebeb87186c8ea8f34bb415c1867f6
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-08-31 22:34:03 +1000

    Add available chars in script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var length = 12;
+
+var numbers = "0123456789";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +20,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Password Gen Function
+function generatePassword(){
+
+}
