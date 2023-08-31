3f36215a70e1cde0831b52e6b3d46dfe15698c6c
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-08-31 22:53:46 +1000

    Added getLength function

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var length = 12;
+var length;
 
-var numbers = "0123456789";
+var numeric = "0123456789";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 
+
+//Prompts order
+// Ask for length input from 8-128 
+// Ask for lowerCase
+// Ask for upperCase
+// Ask for numeric
+// Ask for specialChars
+
+
 // Write password to the #password input
 function writePassword() {
+  length = getLength();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
@@ -21,8 +30,17 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function getLength(){
+  var lengthInput = window.prompt("Input length of password");
+  
+  if (lengthInput < 8){
+    window.alert("Minimum length is 8");
+    
+  }
+}
 
 // Password Gen Function
 function generatePassword(){
-
+  var test = "test";
+  return test;
 }
